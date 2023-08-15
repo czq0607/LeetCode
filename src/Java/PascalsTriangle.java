199884db1e80770026ac1b71be8e8cfc7924fad3
czq <2496026059@qq.com>
2023-08-15 19:56:05 +0800

    7.22(3

---
+++ src/Java/PascalsTriangle.java
@@ -0,0 +1,41 @@
+package Java;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        if (numRows <= 0) {
+            return triangle;
+        }
+
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        triangle.add(firstRow);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = triangle.get(i - 1);
+            List<Integer> newRow = new ArrayList<>();
+            newRow.add(1);
+
+            for (int j = 1; j < i; j++) {
+                newRow.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+
+            newRow.add(1);
+            triangle.add(newRow);
+        }
+
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+        int numRows = 5;  // Replace with the desired number of rows
+        List<List<Integer>> result = generate(numRows);
+
+        for (List<Integer> row : result) {
+            System.out.println(row);
+        }
+    }
+}
---
+++ src/Python/PascalsTriangle.py
@@ -0,0 +1,22 @@
+def generate_pascal_triangle(numRows):
+    if numRows == 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, numRows):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
+
+numRows = 7
+result = generate_pascal_triangle(numRows)
+for row in result:
+    print(row)
